5666c5fc74cc975a2330fe7c595272f9b6bfe644
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2022-08-01 07:39:27 -0400

    Refractored assertEqual and head test code into test files.

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,9 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual !== expected) {
-    console.log(`Asserion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
+const assertEqual = function (actual, expected) {
+	if (actual !== expected) {
+		console.log(`Asserion Failed: ${actual} !== ${expected}`);
+	} else {
+		console.log(`Assertion Passed: ${actual} === ${expected}`);
+	}
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual !== expected) {
-    console.log(`Asserion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
 const head = function(array) {
   if(array.length > 0){
     return array[0];
@@ -14,7 +5,5 @@ const head = function(array) {
   return array;
 }
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([3,5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
+
