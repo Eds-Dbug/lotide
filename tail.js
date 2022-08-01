2d02a68171035e568e789f2a296c3cf5b55c4985
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2022-08-01 07:49:54 -0400

    refactored tail test cases into test file.

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`Asserion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
-
-};
-
 const tail = function(array) {
   if (array.length > 0) {
     return array.slice(1,array.length);
@@ -14,14 +5,5 @@ const tail = function(array) {
   return array;
 };
 
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+module.exports = tail;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
