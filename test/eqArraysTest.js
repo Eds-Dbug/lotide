ab2b68148b1e169843ba97fa04b2e1cdebfee583
Eds-Dbug <83380248+Eds-Dbug@users.noreply.github.com>
2022-08-07 19:27:44 -0400

    Finished eqArrays

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,10 +1,34 @@
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual')
+const {expect} = require('chai');
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+describe('#eqArrays', () => {
+  it('should return true for ["1", "2", "3"] and ["1", "2", "3"]', () => {
+    expect(eqArrays(["1", "2", "3"], ["1", "2", "3"])).to.be.true;
+  })
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  it('should return false for ["1", "2", "3"] and ["1", "2", 3]', () => {
+    expect(eqArrays(["1", "2", "3"], ["1", "2", 3])).to.be.false;
+  })
+
+  it('should return true for [1, 2, 3] and [1, 2, 3]', () => {
+    expect(eqArrays([1, 2, 3], [1, 2, 3])).to.be.true
+  })
+
+  it('should return true for [[2, 3], [4]] and [[2, 3], [4]]', () => {
+    expect(eqArrays([[2, 3], [4]], [[2, 3], [4]])).to.be.true
+  })
+
+  it('should return false for [[2, 3], [4]] and [[2, 3], [4, 5]]', () => {
+    expect(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])).to.be.false
+  })
+
+  it('should return false for [[2, 3], [4]] and [[2, 3], 4]', () => {
+    expect(eqArrays([[2, 3], [4]], [[2, 3], 4])).to.be.false
+  })
+})
+
+
 // => true
-// => false
+
+ // => false
+ // => false
